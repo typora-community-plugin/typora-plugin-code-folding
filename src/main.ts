b938548ae1066e1f71bf6d176435550a628b26cf
plylrnsdy <plylrnsdy@163.com>
2024-10-26 14:22:58 +0800

    fix: can not fold ruby & sql

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import 'codemirror/addon/fold/foldgutter.css'
 import { Plugin, decorate, path } from '@typora-community-plugin/core'
 import { editor } from 'typora'
+import type * as CodeMirror from 'codemirror'
 
+
+const FOLD_BY_INDENT = ['ruby', 'sql']
 
 export default class extends Plugin {
 
+  private _isModeFixed = false
+
   onload() {
+
     this.registerScript('./addon/fold/foldcode.js')
     this.registerScript('./addon/fold/foldgutter.js')
     this.registerScript('./addon/fold/brace-fold.js')
@@ -16,6 +22,19 @@ export default class extends Plugin {
 
     this.register(
       decorate.returnValue(editor.fences, 'addCodeBlock', (_, cm) => {
+        if (!this._isModeFixed) {
+          for (const lang of FOLD_BY_INDENT) {
+            this.register(
+              // @ts-ignore
+              decorate.returnValue(CodeMirror.modes, lang, (_, results) => {
+                // @ts-ignore
+                results.fold = 'indent'
+                return results
+              }))
+          }
+          this._isModeFixed = true
+        }
+
         const gutters = cm.getOption('gutters') ?? []
         // @ts-ignore
         cm.setOption('foldGutter', true)
--- test/vault/doc2.md
+++ test/vault/doc2.md
@@ -1,6 +1,7 @@
 # doc2
 
 ```javascript
+// JavaScript
 function add(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError()
@@ -8,3 +9,34 @@ function add(a, b) {
   return a + b
 }
 ```
+
+```sql
+-- SQL
+SELECT a, b, c
+  FROM table1
+  WHERE id = 1
+```
+
+```ruby
+# Ruby
+class ImportMap::Commands < Thor
+  include Thor.Actions
+  
+  def pin(*)
+    package.download(package, url)
+  end
+  
+  private
+  
+  def packager
+    @package ||= Importer.Packager.new
+  end
+end
+```
+
+```python
+# Python
+def add(a, b):
+  return a + b
+```
+
